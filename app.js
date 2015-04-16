ba392d7dd9a880ebe22e47544a9e88465235710c
Joel Holder <jclosure@gmail.com>
2015-04-16 02:49:15 +0000

    refactored app and modules to work with Express 4

--- app.js
+++ app.js
@@ -6,30 +6,31 @@
 var express = require('express')
   , stylus = require('stylus')
   , redis = require('redis')
-  , http = require('http');
+  , http = require('http')
+  , logger = require('morgan')
+  , errorHandler = require('errorhandler');
 
 app = express();
 
-app.configure(function(){
-  app.db = redis.createClient();
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.set('phantom', 'phantomjs');
-  app.set('screenshots', '/tmp');
-  app.set('default viewport width', 1024);
-  app.set('default viewport height', 600);
-  app.set('colors', 3);
-  app.set('root', __dirname);
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(stylus.middleware({ src: __dirname + '/public' }));
-  app.use(express.static(__dirname + '/public'));
-  app.use(app.router);
-});
 
-app.configure('development', function(){
-  app.use(express.errorHandler()); 
-});
+app.db = redis.createClient();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set('phantom', 'phantomjs');
+app.set('screenshots', '/tmp');
+app.set('default viewport width', 1024);
+app.set('default viewport height', 600);
+app.set('colors', 3);
+app.set('root', __dirname);
+app.use(logger);
+app.use(stylus.middleware({ src: __dirname + '/public' }));
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+app.use(errorHandler()); 
+
 
 require('./routes');
 
